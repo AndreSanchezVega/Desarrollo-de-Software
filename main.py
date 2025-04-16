0d6c675a0736c090e857e641db8ebb2c3626a4f2
Andre <andresan0808@gmail.com>
2025-04-16 08:34:20 -0500

    Guardando cambios antes de cambiar de rama

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 HEAD
 HEAD
 
-
-=======
-<<<<<<< HEAD
-print('Hello World - updated in main')
-=======
->>>>>>> afbf584 (Revert "Revert "Resolve merge conflict between main and feature/advanced-feature"")
 def greet():
     print('Hello from advanced feature')
 
 greet()
-<<<<<<< HEAD
 7b3a721 (Agregando una funcion a main.py)
 368beaf (Revert "Resolve merge conflict between main and feature/advanced-feature")
-=======
->>>>>>> feature/advanced-feature
->>>>>>> afbf584 (Revert "Revert "Resolve merge conflict between main and feature/advanced-feature"")
